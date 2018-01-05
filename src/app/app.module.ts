b586dfda3ba95b53c0b9c0ea0dd967838d3c6652
Miro Wallin <miro.wallin@eng.tamk.fi>
2018-01-05 19:06:36 +0200

    d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { KeskusteluComponent } from './keskustelu/keskustelu.component';
+import { KarttaComponent } from './kartta/kartta.component';
 
-
+const appRoutes: Routes = [
+  {
+    path: 'view1',
+    component: KeskusteluComponent
+  },
+  {
+    path: 'view2',
+    component: KarttaComponent
+  },
+  {
+    path: '',
+    redirectTo: '/view1',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: KeskusteluComponent
+  }
+];
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAx4Sk7dzT-aVWgATsMkmYeb8WUF1umYF4',
+  authDomain: 'tulikanta-b0a63.firebaseapp.com',
+  databaseURL: 'https://tulikanta-b0a63.firebaseio.com',
+  projectId: 'tulikanta-b0a63',
+  storageBucket: '',
+  messagingSenderId: '735330889876'
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    KeskusteluComponent,
+    KarttaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
